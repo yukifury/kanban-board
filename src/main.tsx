5b8f908a34b9ce7f0f177d1899ef5e677479821a
yukifury <forlorn128@gmail.com>
2025-03-27 17:13:40 +0000

    fix: return `<StrictMode />` to `main.tsx`;

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { MainLayout } from './components/layouts/main-layout';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <MainLayout>
-    <App />
-  </MainLayout>,
-  // </StrictMode>,
+  <StrictMode>
+    <MainLayout>
+      <App />
+    </MainLayout>
+  </StrictMode>,
 );
